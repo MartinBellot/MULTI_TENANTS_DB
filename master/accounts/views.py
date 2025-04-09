dd849bbc892314a9916c3ddde40573ae3abd942a
Guillaume Coquemont <gui.coquemont@gmail.com>
2025-04-09 15:36:05 +0200

    mise en place du front

--- master/accounts/views.py
+++ master/accounts/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
+
+def home(request):
+    form = AuthenticationForm()
+    return render(request, 'home.html', {'form': form})
 
 # Create your views here.
--- master/master/urls.py
+++ master/master/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Route pour la page d'accueil
     path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
 ]
 
