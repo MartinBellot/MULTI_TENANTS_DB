38a7226ea929437f96d5e522b882369d49ae0819
Guillaume Coquemont <gui.coquemont@gmail.com>
2025-04-15 16:04:24 +0200

    modif redirection

--- master/accounts/urls.py
+++ master/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from frontend import views as frontend_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('launch-server/', frontend_views.launch_new_server, name='launch_new_server'),
+    path('dashboard/', frontend_views.dashboard, name='dashboard'),
+
 ]
--- master/frontend/views.py
+++ master/frontend/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from .forms import CustomAuthenticationForm
 from django.contrib.auth.decorators import login_required
 from accounts.models import CustomUser
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
 def home(request):
     form = CustomAuthenticationForm(request, data=request.POST or None)
@@ -10,14 +12,24 @@ def home(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('/admin/')
+            return redirect('/dashboard/')
     return render(request, 'home.html', {'form': form})
 
 @login_required
 def dashboard(request):
-    if request.user.is_superuser:
-        all_files = CustomUser.objects.all()
-        return render(request, 'admin:index', {'all_files': all_files})
-    else:
-        user_files = CustomUser.objects.filter(user=request.user)
-        return render(request, 'dashboard.html', {'user_files': user_files})
+    user_files = CustomUser.objects.filter(id=request.user.id)
+    return render(request, 'dashboard.html', {'user_files': user_files})
+    
+@require_http_methods(["GET"])
+def launch_new_server(request):
+    # Ici, vous pouvez ajouter la logique qui lance réellement le serveur
+    # (exemple : appeler un script, démarrer un processus, etc.)
+    return HttpResponse("Le nouveau serveur a été lancé avec succès !")
+
+@require_http_methods(["GET", "POST"])
+def create_tenant(request):
+    if request.method == "POST":
+        # Traitement de la création d'un tenant à partir des données envoyées par le formulaire
+        # Par exemple : validation et sauvegarde d’un tenant dans la BDD
+        return HttpResponse("Tenant créé avec succès !")
+    return render(request, "create_tenant.html")
