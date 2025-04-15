ee087e341f3d874019c1063a2c25708ec76729ca
Paul Delhomme <36136537+PavelDelhomme@users.noreply.github.com>
2025-04-15 10:45:14 +0200

    Génération partiel des secret key de djanso dans tenant et dans master

--- master/entrypoint.sh
+++ master/entrypoint.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 set -e
 
+ENV_FILE=".env"
+
+generate_secret_key() {
+  python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
+}
+
+# === GÉNÉRATION DE LA SECRET KEY AVANT TOUT ===
+echo "=== Checking DJANGO_SECRET_KEY ==="
+if ! grep -q "^DJANGO_SECRET_KEY=" "$ENV_FILE"; then
+    echo "DJANGO_SECRET_KEY missing, generating one..."
+    GENERATED_SECRET_KEY=$(generate_secret_key)
+    echo "" >> "$ENV_FILE"
+    echo "DJANGO_SECRET_KEY=$GENERATED_SECRET_KEY" >> "$ENV_FILE"
+fi
+
+# Recharge la clé pour l’environnement actuel
+export DJANGO_SECRET_KEY=$(grep "^DJANGO_SECRET_KEY=" "$ENV_FILE" | cut -d '=' -f2-)
+
 echo "Waiting for database at $DB_HOST:$DB_PORT..."
-# Attendre que le service de base de données soit accessible
 while ! nc -z $DB_HOST $DB_PORT; do
   echo "Database not ready, waiting..."
   sleep 1
@@ -23,6 +40,7 @@ User = get_user_model()
 if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
     print("Creating superuser...")
     User.objects.create_superuser(
+        username='$DJANGO_SUPERUSER_USERNAME',
         email='$DJANGO_SUPERUSER_EMAIL',
         password='$DJANGO_SUPERUSER_PASSWORD'
     )
